1eae2bd2cb8d133a851184bd5bddbc874bf9493d
Gabe Berke-Williams <gbw@brandeis.edu>
2009-10-03 15:38:47 -0400

    Added documentation. service_name is now an instance variable (@service_name) so that it's actually used instead of being translated as a blank string

--- lib/pidgin2adium/log_generator.rb
+++ lib/pidgin2adium/log_generator.rb
@@ -2,7 +2,7 @@ module Pidgin2Adium
     class LogGenerator
 	include Pidgin2Adium
 	def initialize(service, user_SN, partner_SN, adium_chat_time_start, dest_dir_base, force=false)
-	    @service = service
+	    # service is used below
 	    @user_SN = user_SN
 	    @partner_SN = partner_SN
 	    @adium_chat_time_start = adium_chat_time_start
@@ -20,19 +20,20 @@ module Pidgin2Adium
 		'msn' => 'MSN',
 		'yahoo' => 'Yahoo'}
 	    
-	    service_name = @SERVICE_NAME_MAP[@service.downcase]
-	    dest_dir_real = File.join(@dest_dir_base, "#{service_name}.#{@user_SN}", @partner_SN, "#{@partner_SN} (#{@adium_chat_time_start}).chatlog")
+	    @service_name = @SERVICE_NAME_MAP[service.downcase]
+	    dest_dir_real = File.join(@dest_dir_base, "#{@service_name}.#{@user_SN}", @partner_SN, "#{@partner_SN} (#{@adium_chat_time_start}).chatlog")
 	    FileUtils.mkdir_p(dest_dir_real)
 	    @dest_file_path = dest_dir_real << '/' << "#{@partner_SN} (#{@adium_chat_time_start}).xml"
 	end
 
+	# :nodoc:
 	def file_exists?
 	    return File.exist?(@dest_file_path)
 	end
 
+	# Given an array of Message, Status, and/or Event objects created by LogParser, generates
 	# Returns path of output file.
 	def generate(chat_array)
-	    # chat_array is an array of Message, Status, and Event objects created by LogParser
 	    if not @force
 		return FILE_EXISTS if file_exists?
 	    end
@@ -40,12 +41,11 @@ module Pidgin2Adium
 	    # TODO: inject? map! ?
 	    chat_array.each { |obj| all_msgs << obj.to_s }
 	    
-	    # no \n before </chat> because all_msgs has it already
-	    ret = sprintf('<?xml version="1.0" encoding="UTF-8" ?>'<<"\n"+
-		      '<chat xmlns="http://purl.org/net/ulf/ns/0.4-02" account="%s" service="%s">'<<"\n"<<'%s</chat>', @user_SN, service_name, all_msgs)
-
 	    outfile = File.new(@dest_file_path, 'w')
-	    outfile.puts(ret)
+	    # no \n before </chat> because all_msgs has it already
+	    outfile.printf('<?xml version="1.0" encoding="UTF-8" ?>'<<"\n"+
+		   '<chat xmlns="http://purl.org/net/ulf/ns/0.4-02" account="%s" service="%s">'<<"\n"<<'%s</chat>',
+		   @user_SN, @service_name, all_msgs)
 	    outfile.close
 	    return @dest_file_path
 	end
