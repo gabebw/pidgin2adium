166429f119935e37e0af27d2302e85cb2cafbe17
Gabe Berke-Williams <gbw@brandeis.edu>
2009-10-10 20:44:50 -0400

    Pass an actual options hash to parse_and_generate, instead of a boolean which it ignores.

--- lib/pidgin2adium/log_converter.rb
+++ lib/pidgin2adium/log_converter.rb
@@ -1,13 +1,23 @@
 require 'pidgin2adium'
 
 module Pidgin2Adium
-    # An easy way to batch-process a directory. Used by the pidgin2adium command-line script.
+    # An easy way to batch-process a directory. Used by the pidgin2adium
+    # command-line script.
     class LogConverter
 	include Pidgin2Adium
-	def initialize(pidgin_log_dir, aliases, overwrite = false)
+	# You can add options using the _opts_ hash, which can have the
+	# following keys, all of which are optional:
+	# * *overwrite*: If true, then overwrite even if log is found.
+	#   Defaults to false.
+	# * *output_dir*: The top-level dir to put the logs in.
+	#   Logs under output_dir are still each in their own folders, etc.
+	#   Defaults to Pidgin2Adium::ADIUM_LOG_DIR
+	def initialize(pidgin_log_dir, aliases, opts = {})
+	    # parse_and_generate will process it for us
+	    @opts = opts
+
 	    @pidgin_log_dir = File.expand_path(pidgin_log_dir)
 	    @my_aliases = aliases
-	    @overwrite = overwrite
 
 	    unless File.directory?(@pidgin_log_dir)
 		puts "Source directory #{@pidgin_log_dir} does not exist or is not a directory."
@@ -36,7 +46,7 @@ module Pidgin2Adium
 		    sprintf("[%d/%d] Converting %s...",
 			(i+1), total_files, fname)
 		)
-		result = parse_and_generate(fname, @my_aliases, @overwrite)
+		result = parse_and_generate(fname, @my_aliases, @opts)
 		total_successes += 1 if result == true
 	    end
 
