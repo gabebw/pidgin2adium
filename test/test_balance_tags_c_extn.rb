32bd499f239e44f6981a84227aa628fe243ecd91
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-05 15:32:14 -0700

    Add some tests

--- test/test_balance_tags_c_extn.rb
+++ test/test_balance_tags_c_extn.rb
@@ -4,7 +4,20 @@ $:.unshift File.dirname(__FILE__) + "/../ext/balance_tags_c"
 require "balance_tags_c.so"
 
 class TestBalanceTagsCExtn < Test::Unit::TestCase
-  def test_truth
-    assert true
+  def test_no_balance
+    assert_equal "asdf", Pidgin2Adium.balance_tags_c('asdf')
+  end
+
+  def test_with_balance
+    assert_equal "<p><b>this is unbalanced!</b></p>",
+      Pidgin2Adium.balance_tags_c('<p><b>this is unbalanced!')
+  end
+
+  # Make sure it doesn't segfault
+  def test_run_lots_of_times
+    100_000.times do
+      assert_equal "<p><b>this is unbalanced!</b></p>",
+        Pidgin2Adium.balance_tags_c('<p><b>this is unbalanced!')
+    end
   end
 end
