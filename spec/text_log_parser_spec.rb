04b1c33eaa8da35f5fabd1ac823d893c410f702e
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-09 13:03:17 -0700

    Add basic spec for TextLogParser#parse

--- spec/text_log_parser_spec.rb
+++ spec/text_log_parser_spec.rb
@@ -3,14 +3,40 @@ require 'spec_helper'
 describe "TextLogParser" do
   before(:each) do
     @time = '(04:20:06)'
+    @tlp = Pidgin2Adium::TextLogParser.new(@text_logfile_path,
+                                           @aliases)
   end
   it "should cleanup text correctly" do
     dirty_text = %Q{\r\n#{@time}&<b>Hello!</b> "Hi!" 'Oh no'\n}
     # "\n" not removed if it ends a line or is followed by
     # a timestamp
     clean_text = %Q{\n#{@time}&amp;&lt;b&gt;Hello!&lt;/b&gt; &quot;Hi!&quot; &apos;Oh no&apos;\n}
-    tlp = Pidgin2Adium::TextLogParser.new(@html_logfile_path,
-                                          @aliases)
-    tlp.cleanup(dirty_text).should == clean_text
+    @tlp.cleanup(dirty_text).should == clean_text
+  end
+
+  describe "#parse" do
+    it "should return a LogFile instance" do
+      @tlp.parse().should be_instance_of(Pidgin2Adium::LogFile)
+    end
+
+    it "should return a LogFile with the correct number of chat_lines" do
+      logfile = @tlp.parse
+      logfile.chat_lines.size.should == 1
+    end
+
+    it "should return a LogFile with the correct message type" do
+      logfile = @tlp.parse
+      logfile.chat_lines[0].should be_instance_of(Pidgin2Adium::XMLMessage)
+    end
+
+    it "should return a LogFile with the correct data" do
+      logfile = @tlp.parse
+      msg = logfile.chat_lines[0]
+      msg.sender.should == "awesomesn"
+      msg.body.should == "what are you doing tomorrow?"
+      msg.buddy_alias.should == "Gabe B-W"
+      # Use regex to ignore time zone
+      msg.time.should =~ /^2006-12-21T22:36:11[-+]\d{2}00$/
+    end
   end
 end
