4ccab5e9e9ee52416993aa68f6a2039bb38a0a11
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-26 16:59:00 -0400

    Bump Pidgin2Adium::VERSION to 3.1.1

--- lib/pidgin2adium.rb
+++ lib/pidgin2adium.rb
@@ -13,7 +13,7 @@ module Pidgin2Adium
   ADIUM_LOG_DIR = File.expand_path('~/Library/Application Support/Adium 2.0/Users/Default/Logs/') << '/'
   # These files/directories show up in Dir.entries()
   BAD_DIRS = %w{. .. .DS_Store Thumbs.db .system}
-  VERSION = "3.1.0"
+  VERSION = "3.1.1"
   # For displaying after we finish converting
   @@oops_messages = []
   @@error_messages = []
