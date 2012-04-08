ebaebae347863fa545bbd4884253fcca807d0106
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-04-08 14:33:34 -0400

    Big cleanup of test suite.

--- spec/pidgin2adium/log_file_spec.rb
+++ spec/pidgin2adium/log_file_spec.rb
@@ -1,63 +1,62 @@
 require 'spec_helper'
 
-describe "LogFile" do
+describe Pidgin2Adium::LogFile do
+  let(:sender_screen_name) { 'gabebw' }
+  let(:sender_alias) { 'Gabe B-W' }
+  let(:receiver_screen_name) { 'matz' }
+  let(:receiver_alias) { 'Yukihiro Matsumoto' }
+  let(:times) do
+    ['2010-08-10T22:55:07-0500',
+     '2010-08-10T22:55:12-0500',
+     '2010-08-10T22:55:17-0500',
+     '2010-08-10T22:55:22-0500'].map { |string| Time.parse(string) }
+  end
+
   before do
-    @sender_screen_name = "gabebw"
-    @user_alias = "Gabe B-W"
+    message_1 = Pidgin2Adium::XMLMessage.new(sender_screen_name, times[0],
+                                             sender_alias, 'Hello!')
+    message_2 = Pidgin2Adium::StatusMessage.new(receiver_screen_name, times[1],
+                                                receiver_alias, 'Matz has gone away')
 
-    @receiver_screen_name = "matz"
-    @partner_alias = "Yukihiro Matsumoto"
+    message_3 = Pidgin2Adium::Event.new(sender_screen_name, times[2], sender_alias,
+                                        'gabebw logged in.', 'online')
 
-    @start_time = "2010-08-10T22:55:07-0500"
-    times = [@start_time,
-             "2010-08-10T22:55:12-0500",
-             "2010-08-10T22:55:17-0500",
-             "2010-08-10T22:55:22-0500"].map { |string| Time.parse(string) }
-
-    message_1 = Pidgin2Adium::XMLMessage.new(@sender_screen_name, times[0],
-                                             @user_alias, "Hello!")
-    message_2 = Pidgin2Adium::StatusMessage.new(@receiver_screen_name, times[1],
-                                                @partner_alias, "Matz has gone away")
-
-    message_3 = Pidgin2Adium::Event.new(@sender_screen_name, times[2], @user_alias,
-                                        "gabebw logged in.", 'online')
-
-    message_4 = Pidgin2Adium::AutoReplyMessage.new(@receiver_screen_name, times[3],
-                                                   @partner_alias,
-                                                   "This is an away message")
+    message_4 = Pidgin2Adium::AutoReplyMessage.new(receiver_screen_name, times[3],
+                                                   receiver_alias,
+                                                   'This is an away message')
 
     @messages = [message_1, message_2, message_3, message_4]
     @logfile = Pidgin2Adium::LogFile.new(@messages)
   end
 
-  describe "#to_s" do
-    it "should return the correct string" do
+  describe '#to_s' do
+    it 'should return the correct string' do
       @logfile.to_s.should == @messages.map(&:to_s).join
     end
   end
 
 
-  describe "enumerable methods" do
-    it "should include Enumerable" do
+  describe 'enumerable methods' do
+    it 'should include Enumerable' do
       Pidgin2Adium::LogFile.included_modules.include?(Enumerable).should be_true
     end
 
-    describe "#each_with_index" do
-      it "yields the correct messages, in order" do
-        @logfile.each_with_index do |msg, n|
-          msg.should == @messages[n]
+    describe '#each_with_index' do
+      it 'yields the correct messages, in order' do
+        @logfile.each_with_index do |message, n|
+          message.should == @messages[n]
         end
       end
     end
 
-    describe "#max" do
-      it "returns the most recent message" do
+    describe '#max' do
+      it 'returns the most recent message' do
         @logfile.max.should == @messages.last
       end
     end
 
-    describe "#min" do
-      it "returns the oldest message" do
+    describe '#min' do
+      it 'returns the oldest message' do
         @logfile.min.should == @messages.first
       end
     end
--- spec/pidgin2adium/xml_message_spec.rb
+++ spec/pidgin2adium/xml_message_spec.rb
@@ -1,61 +1,57 @@
 require 'spec_helper'
 
 describe Pidgin2Adium::XMLMessage, '#to_s' do
-  let(:sender_screen_name) { 'jim_sender' }
-  let(:time) { Time.now }
-  let(:sender_alias) { 'jane_alias' }
-  let(:body) { 'body' }
-
   it 'has the correct sender_screen_name' do
-    create_xml_message.to_s.should include %(sender="#{sender_screen_name}")
+    create_xml_message(:sender_screen_name => 'jim').to_s.should include %(sender="jim")
   end
 
   it 'has the correct alias' do
-    create_xml_message.to_s.should include %(alias="#{sender_alias}")
+    create_xml_message(:sender_alias => 'Jim Alias').to_s.should include %(alias="Jim Alias")
   end
 
   it 'has the correct time' do
     time = Time.now
     formatted_time = time.strftime('%Y-%m-%dT%H:%M:%S%Z')
-    result = Pidgin2Adium::XMLMessage.new(sender_screen_name, time, sender_alias, body).to_s
-    result.should include %(time="#{formatted_time}")
+    create_xml_message(:time => time).to_s.should include %(time="#{formatted_time}")
   end
 
   it 'has the correct body' do
-    styled_body = %(<div><span style="font-family: Helvetica; font-size: 12pt;">#{body}</span></div>)
-    create_xml_message.to_s.should include styled_body
+    unstyled_body = 'unstyled'
+    styled_body = %(<div><span style="font-family: Helvetica; font-size: 12pt;">#{unstyled_body}</span></div>)
+    create_xml_message(:body => unstyled_body).to_s.should include styled_body
   end
 
   context "normalization" do
     it 'balances the tags in the body' do
       unbalanced_body = '<div>unbalanced'
-      balanced_message = Pidgin2Adium::XMLMessage.new(sender_screen_name, time, sender_alias, unbalanced_body)
-      balanced_message.body.should == '<div>unbalanced</div>'
+      create_xml_message(:body => unbalanced_body).body.should == '<div>unbalanced</div>'
     end
 
-    it 'removes *** from the beginning of the buddy alias' do
+    it 'removes *** from the beginning of the sender alias' do
       alias_with_stars = '***Jim'
       alias_without_stars = 'Jim'
-      xml_message = Pidgin2Adium::XMLMessage.new(sender_screen_name, time, alias_with_stars, body)
-      xml_message.to_s.should include %(alias="#{alias_without_stars}")
+      create_xml_message(:sender_alias => alias_with_stars).to_s.should include %(alias="#{alias_without_stars}")
     end
 
     it 'escapes & to &amp;' do
-      ampersand = '&'
-      xml_message = Pidgin2Adium::XMLMessage.new(sender_screen_name, time, sender_alias, ampersand)
-      xml_message.body.should == '&amp;'
+      create_xml_message(:body => '&').body.should == '&amp;'
     end
 
     %w(lt gt amp quot apos).each do |entity|
       it "does not escape &#{entity};" do
         entity_body = "&#{entity};"
-          xml_message = Pidgin2Adium::XMLMessage.new(sender_screen_name, time, sender_alias, entity_body)
-        xml_message.body.should == entity_body
+        create_xml_message(:body => entity_body).body.should == entity_body
       end
     end
   end
 
-  def create_xml_message
-    Pidgin2Adium::XMLMessage.new(sender_screen_name, time, sender_alias, body)
+  def create_xml_message(opts = {})
+    opts[:sender_screen_name] ||= 'jim_sender'
+    opts[:time] ||= Time.now
+    opts[:sender_alias] ||= 'jane_alias'
+    opts[:body] ||= 'body'
+    opts[:event_type] ||= 'libPurpleEvent'
+
+    Pidgin2Adium::XMLMessage.new(opts[:sender_screen_name], opts[:time], opts[:sender_alias], opts[:body])
   end
 end
