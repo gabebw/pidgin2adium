2c8f0de946e52434be5d7b39567c277591b18185
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-04-08 13:23:09 -0400

    Remove some comments.

--- lib/pidgin2adium/messages/message.rb
+++ lib/pidgin2adium/messages/message.rb
@@ -1,14 +1,11 @@
-# The Message class and its subclasses, each used for holding one line of a
-# chat.
-
 module Pidgin2Adium
   # A holding object for each line of the chat. It is subclassed as
   # appropriate (eg AutoReplyMessage). Each subclass (but not Message
   # itself) has its own to_s which prints out its information in a format
   # appropriate for putting in an Adium log file.
-  # Subclasses: XMLMessage, AutoReplyMessage, StatusMessage, Event.
   class Message
     include Comparable
+
     def initialize(sender, adium_formatted_time, buddy_alias)
       # The sender's screen name
       @sender = sender
@@ -21,9 +18,6 @@ module Pidgin2Adium
     attr_reader :sender, :time, :buddy_alias
 
     # Compare this Message to +other_message+, based on their timestamps.
-    # Returns a number < 0 if this message was sent before +other_message+,
-    # 0 if they were sent at the same time, and a number > 0 if this message
-    # was sent after +other_message+.
     def <=>(other_message)
       @time_object - other_message.time_object
     end
