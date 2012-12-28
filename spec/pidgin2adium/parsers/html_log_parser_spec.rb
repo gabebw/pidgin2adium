3c7f68b122afb894b50388f30f1b717df1155f84
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-12-28 03:02:03 -0500

    Break up HTML parser spec

--- spec/pidgin2adium/parsers/html_log_parser_spec.rb
+++ spec/pidgin2adium/parsers/html_log_parser_spec.rb
@@ -1,70 +1,91 @@
 describe Pidgin2Adium::HtmlLogParser do
-  describe "#parse" do
-    let(:path) do
-      create_chat_file('parse.html') do |b|
-        b.first_line from: 'otherSN', to: 'aolsystemmsg',
-          time: '1/15/2008 7:14:45 AM'
-        b.message time: '2008-01-15 07:14:45',
-          from_alias: 'AOL System Msg',
-          text: %{Your screen name (otherSN) is now signed into AOL(R) Instant Messenger (TM) in 2 locations. To sign off the other location(s), reply to this message with the number 1. Click <a href='http://www.aim.com/password/routing.adp'>here</a> for more information.},
-          font_color: 'A82F2F'
-        b.message time: '2008-01-15 07:14:48', from_alias: 'Gabe B-W',
-          text: %{<span style='color: #000000;'>1</span>},
-          font_color: '16569E'
-        b.message time: '2008-01-15 07:14:48',
-          from_alias: 'AOL System Msg',
-          text: %{Your other AIM sessions have been signed-off.  You are now signed-on from 1 location(s).},
-          font_color: 'A82F2F'
+  context '#parse' do
+    it 'returns a Chat with the correct number of lines' do
+      chat = build_chat do |b|
+        b.first_line
+        3.times { b.message }
       end
+
+      chat.lines.size.should == 3
     end
 
-    before do
-      @chat = create_parser_for(path, 'Gabe B-W').parse
+    it 'returns a Chat with the correct message type' do
+      chat = build_chat do |b|
+        b.first_line
+        b.message 'first'
+        b.message 'second'
+      end
+
+      chat.lines.map(&:class).should == [Pidgin2Adium::XMLMessage] * 2
     end
 
-    it "returns a Chat instance" do
-      @chat.should be_instance_of(Pidgin2Adium::Chat)
+    it 'parses out the screen name for the user who is doing the logging' do
+      message = build_chat('Gabe B-W') do |b|
+        b.first_line from: 'from', to: 'to'
+        b.message 'whatever', from: 'from', from_alias: 'Gabe B-W'
+      end.lines.first
+
+      message.sender_screen_name.should == 'from'
     end
 
-    it "returns a Chat with the correct number of lines" do
-      @chat.lines.size.should == 3
+    it 'parses out the alias for the user who is doing the logging' do
+      message = build_chat do |b|
+        b.first_line from: 'from', to: 'to'
+        b.message 'whatever', from_alias: 'Jack Alias'
+      end.lines.first
+
+      message.sender_alias.should == 'Jack Alias'
     end
 
-    it "returns a Chat with the correct message type" do
-      @chat.lines.map(&:class).should == [Pidgin2Adium::XMLMessage] * 3
+    it 'parses out the screen name for the user on the other end' do
+      message = build_chat('my-alias') do |b|
+        b.first_line from: 'from'
+        b.message 'whatever', from: 'from', from_alias: 'my-alias'
+      end.lines.first
+
+      message.sender_screen_name.should == 'from'
     end
 
-    it "returns a Chat with the correct data" do
-      first_msg = @chat.lines[0]
-      second_msg = @chat.lines[1]
-      third_msg = @chat.lines[2]
+    it 'parses out the alias for the user on the other end' do
+      message = build_chat('my-alias') do |b|
+        b.first_line from: 'from'
+        b.message 'whatever', from: 'from', from_alias: 'my-alias'
+      end.lines.first
 
-      first_msg.sender_screen_name.should == "aolsystemmsg"
-      first_msg.sender_alias.should == "AOL System Msg"
-      first_msg.time.should == DateTime.parse('2008-01-15 07:14:45')
-      # This fails due to balance_tags_c().
-      good_body = %Q{Your screen name (otherSN) is now signed into AOL(R) Instant Messenger (TM) in 2 locations.} + " " +
-        %Q{To sign off the other location(s), reply to this message with the number 1.} + " " +
-        %Q{Click <a href="http://www.aim.com/password/routing.adp">here</a> for more information.}
-      first_msg.body.should == good_body
+      message.sender_alias.should == 'my-alias'
+    end
 
-      second_msg.sender_screen_name.should == "othersn"
-      second_msg.sender_alias.should == "Gabe B-W"
-      second_msg.time.should == DateTime.parse('2008-01-15 07:14:48')
-      second_msg.body.should == "1"
+    it 'parses out the time' do
+      message = build_chat do |b|
+        b.first_line
+        b.message 'whatever', time: '2008-01-15 07:14:45'
+      end.lines.first
 
-      third_msg.sender_screen_name.should == "aolsystemmsg"
-      third_msg.sender_alias.should == "AOL System Msg"
-      third_msg.time.should == DateTime.parse('2008-01-15 07:14:48')
-      third_msg.body.should == "Your other AIM sessions have been signed-off.  You are now signed-on from 1 location(s)."
+      message.time.should == DateTime.parse('2008-01-15 07:14:45')
+    end
+
+    it 'parses out the body' do
+      message = build_chat do |b|
+        b.first_line
+        b.message 'body'
+      end.lines.first
+
+      message.body.should == 'body'
+    end
+
+    it 'double quotes hrefs in the body' do
+      body_with_link = %q(<a href='http://google.com'>first</a>)
+      message = build_chat do |b|
+        b.first_line
+        b.message body_with_link
+      end.lines.first
+
+      message.body.should == body_with_link.gsub("'", '"')
     end
   end
 
-  def create_parser
-    create_parser_for(create_chat_file('dirty.html'))
-  end
-
-  def create_parser_for(file, aliases = '')
-    Pidgin2Adium::HtmlLogParser.new(file, aliases)
+  def build_chat(aliases = 'Gabe B-W', &block)
+    path = create_chat_file('file.html', &block)
+    Pidgin2Adium::HtmlLogParser.new(path, aliases).parse
   end
 end
--- spec/support/chat_builder.rb
+++ spec/support/chat_builder.rb
@@ -1,18 +1,14 @@
-=begin
-For building chats - like factory_girl, but using the builder pattern.
-Ideally it'll work something like this:
-
-# determine format from passed-in file
-ChatBuilder.new('file.html') do |b|
-  b.first_line
-  b.message from: "name1", from_alias: "Gabe B-W",
-         time: '2010-01-30', text: "blarg balrg"
-  b.message from: "name2", from_alias: "another name",
-         text: "balrg blarg", time: '2010-01-30'
-  b.away_message
-  b.status_message
-end
-=end
+# Usage:
+# # determine format from passed-in file
+# ChatBuilder.new('file.html') do |b|
+#   b.first_line
+#   b.message from: 'name1', from_alias: 'Gabe B-W',
+#          time: '2010-01-30', text: 'blarg balrg'
+#   b.message 'blerg' from: 'name2',
+#     from_alias: 'another name', time: '2010-01-30'
+#   b.away_message
+#   b.status_message
+# end
 
 module ChatBuilderMacros
   SPEC_DIR = File.dirname(File.dirname(__FILE__))
@@ -43,6 +39,8 @@ module ChatBuilderMacros
 end
 
 class ChatBuilder
+  DEFAULT_FROM = 'FROM_SN'
+
   def initialize(path)
     @path = path
     @file = File.new(path, 'w')
@@ -62,43 +60,40 @@ end
 class TextChatBuilder < ChatBuilder
   def first_line(options = {})
     @first_line ||= begin
-      to = options[:to] || "TO"
-      time = options[:time] || Time.now.strftime("%Y-%m-%d %H:%M:%S")
-      protocol = options[:protocol] || "aim"
-      from = options[:from] || "FROM_SN"
+      to = options[:to] || 'TO'
+      time = options[:time] || Time.now.strftime('%Y-%m-%d %H:%M:%S')
+      protocol = options[:protocol] || 'aim'
+      from = options[:from] || DEFAULT_FROM
       "Conversation with #{to} at #{time} on #{from} (#{protocol})"
     end
   end
 
   def message(options = {})
-    time = options[:time] || Time.now.strftime("%H:%M:%S")
-    from_alias = options[:from_alias] || "FROM"
-    text = options[:text] || "What's up?"
+    time = options[:time] || Time.now.strftime('%H:%M:%S')
+    from_alias = options[:from_alias] || 'FROM'
+    text = options[:text] || 'Hi there'
     message = "(#{time}) #{from_alias}: #{text}"
     @messages << message
   end
 end
 
 class HtmlChatBuilder < ChatBuilder
-  DEFAULT_FROM = "FROM_SN"
-
   def first_line(options = {})
     @first_line ||= begin
-      to = options[:to] || "TO_SN"
-      time = options[:time] || Time.now.strftime("%m/%d/%Y %I:%M:%S %p")
-      protocol = options[:protocol] || "aim"
+      to = options[:to] || 'TO_SN'
+      time = options[:time] || Time.now.strftime('%m/%d/%Y %I:%M:%S %p')
+      protocol = options[:protocol] || 'aim'
       # Need to track this so we can set the message font color correctly.
       @from = options[:from] || DEFAULT_FROM
       %(<head><meta http-equiv="content-type" content="text/html; charset=UTF-8"><title>Conversation with #{to} at #{time} on #{@from} (#{protocol})</title></head><h3>Conversation with #{to} at #{time} on #{@from} (#{protocol})</h3>)
     end
   end
 
-  def message(options = {})
+  def message(text = 'hello', options = {})
     from = options[:from] || DEFAULT_FROM
-    from_alias = options[:from_alias] || "FROM_ALIAS"
-    time = options[:time] || Time.now.strftime("%Y-%m-%d %H:%M:%D")
-    text = options[:text] || "What's up?"
-    font_color = "#" + (options[:font_color] || font_color_for(from))
+    from_alias = options[:from_alias] || 'FROM_ALIAS'
+    time = options[:time] || Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    font_color = '#' + (options[:font_color] || font_color_for(from))
     message = %{<font color="#{font_color}"><font size="2">(#{time})</font> <b>#{from_alias}</b></font> #{text}<br/>}
     @messages << message
   end
@@ -107,9 +102,9 @@ class HtmlChatBuilder < ChatBuilder
 
   def font_color_for(from)
     if from == @from
-      "A82F2F"
+      'A82F2F'
     else
-      "16569E"
+      '16569E'
     end
   end
 end
