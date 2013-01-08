497d92f27b6e960f43fa0d0a4fa7bb9af1012607
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-01-08 01:15:12 -0500

    Remove shoulds from test descriptions

--- spec/pidgin2adium/tag_balancer_spec.rb
+++ spec/pidgin2adium/tag_balancer_spec.rb
@@ -1,15 +1,15 @@
 describe Pidgin2Adium::TagBalancer do
-  describe "text without tags" do
-    it "should be left untouched" do
-      text = "Foo bar baz, this is my excellent test text!"
+  describe 'text without tags' do
+    it 'is left untouched' do
+      text = 'foo!'
       Pidgin2Adium::TagBalancer.new(text).balance.should == text
     end
   end
 
-  describe "text with tags" do
-    it "should be balanced correctly" do
+  describe 'text with tags' do
+    it 'is balanced correctly' do
       unbalanced = '<p><b>this is unbalanced!'
-      balanced   = "<p><b>this is unbalanced!</b></p>"
+      balanced = '<p><b>this is unbalanced!</b></p>'
       Pidgin2Adium::TagBalancer.new(unbalanced).balance.should == balanced
     end
   end
