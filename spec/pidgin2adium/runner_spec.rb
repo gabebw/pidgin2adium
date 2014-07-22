991d60b43e7328da03050e5d410c0df1807bead5
Gabe Berke-Williams <gabebw@gabebw.com>
2014-07-22 10:09:46 -0400

    Refactor spec

--- spec/pidgin2adium/runner_spec.rb
+++ spec/pidgin2adium/runner_spec.rb
@@ -10,13 +10,22 @@ describe Pidgin2Adium::Runner do
     creator = double(create: nil)
     allow(Pidgin2Adium::FileFinder).to receive(:new).
       with(path_to_directory).and_return(file_finder)
-    allow(Pidgin2Adium::AdiumChatFileCreator).to receive(:new).
-      and_return(creator)
+
+    chat = stubbed_chat
 
     run_runner(path_to_directory)
 
-    expect(Pidgin2Adium::AdiumChatFileCreator).to have_received(:new).with(path_to_file)
-    expect(creator).to have_received(:create)
+    path = "#{adium_log_directory}/aim.me/them/them (#{chat.start_time_xmlschema}).chatlog/them (#{chat.start_time_xmlschema}).xml"
+
+    expect(File.exist?(path)).to be true
+  end
+
+  def path_to_file
+    File.join(path_to_directory, "gabebw", "in.html")
+  end
+
+  def path_to_directory
+    File.expand_path("./in-logs/")
   end
 
   def adium_log_directory
@@ -26,5 +35,17 @@ describe Pidgin2Adium::Runner do
   def run_runner(path)
     runner = Pidgin2Adium::Runner.new(path)
     runner.run
+  end
+
+  def stubbed_chat
+    timestamp = Time.now.xmlschema
+    chat = double(
+      my_screen_name: "me",
+      their_screen_name: "them",
+      start_time_xmlschema: timestamp,
+      service: "aim",
+    )
+    allow(Pipio::Chat).to receive(:new).and_return(chat)
+    chat
   end
 end
