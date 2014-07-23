d3c375cfc4e1901d543b8ef8b1fc28f48a3a1afc
Gabe Berke-Williams <gabebw@gabebw.com>
2014-07-22 21:33:54 -0400

    Continue building out AdiumChatFileCreator and its specs

--- lib/pidgin2adium/adium_chat_file_creator.rb
+++ lib/pidgin2adium/adium_chat_file_creator.rb
@@ -4,18 +4,36 @@ module Pidgin2Adium
       @file_path = file_path
     end
 
-    def create
-      path = Pidgin2Adium::ADIUM_LOG_DIRECTORY.join(
+    def create_file
+      FileUtils.mkdir_p(File.dirname(path))
+      FileUtils.touch(path)
+    end
+
+    def write_file
+      File.open(path, 'w') do |file|
+        file.puts prolog
+        file.puts chat_tag
+      end
+    end
+
+    private
+
+    def path
+      Pidgin2Adium::ADIUM_LOG_DIRECTORY.join(
         "#{chat.service}.#{chat.my_screen_name}",
         chat.their_screen_name,
         "#{chat.their_screen_name} (#{chat.start_time_xmlschema}).chatlog",
         "#{chat.their_screen_name} (#{chat.start_time_xmlschema}).xml"
       )
-      FileUtils.mkdir_p(File.dirname(path))
-      FileUtils.touch(path)
     end
 
-    private
+    def prolog
+      %(<?xml version="1.0" encoding="UTF-8" ?>)
+    end
+
+    def chat_tag
+      %(<chat xmlns="http://purl.org/net/ulf/ns/0.4-02" account="#{chat.my_screen_name}" service="#{chat.service}" adiumversion="1.5.9">)
+    end
 
     def chat
       @chat ||= Pipio::Chat.new(@file_path)
--- spec/pidgin2adium/adium_chat_file_creator_spec.rb
+++ spec/pidgin2adium/adium_chat_file_creator_spec.rb
@@ -1,21 +1,64 @@
 require "spec_helper"
 
 describe Pidgin2Adium::AdiumChatFileCreator do
-  it "parses a Pidgin-formatted logfile and outputs it to the Adium log directory" do
-    time = Time.now.xmlschema
+  it "creates a file in the correct place" do
+    chat = stub_chat
 
-    chat = double(
+    chat_file_creator = Pidgin2Adium::AdiumChatFileCreator.new(path_to_file)
+    chat_file_creator.create_file
+
+    expect(File.exist?(path_for(chat))).to be true
+  end
+
+  it "writes the correct prolog" do
+    write_file
+
+    expect(file_contents.first).to eq %(<?xml version="1.0" encoding="UTF-8" ?>\n)
+  end
+
+  it "writes the correct opening <chat> tag" do
+    write_file
+
+    second_line = file_contents[1]
+
+    expect(second_line).to eq(
+      %(<chat xmlns="http://purl.org/net/ulf/ns/0.4-02" account="#{chat.my_screen_name}" service="#{chat.service}" adiumversion="1.5.9">\n)
+    )
+  end
+
+  def stub_chat(new_chat = chat)
+    allow(Pipio::Chat).to receive(:new).and_return(new_chat)
+    new_chat
+  end
+
+  def chat(options = {})
+    timestamp = Time.now.xmlschema
+    double({
       my_screen_name: "me",
       their_screen_name: "them",
-      start_time_xmlschema: time,
+      start_time_xmlschema: timestamp,
       service: "aim",
+      lines: %w(a b c)
+    }.merge(options))
+  end
+
+  def file_contents
+    File.readlines(path_for(chat))
+  end
+
+  def write_file
+    chat = stub_chat
+    chat_file_creator = Pidgin2Adium::AdiumChatFileCreator.new(path_to_file)
+    chat_file_creator.create_file
+    chat_file_creator.write_file
+  end
+
+  def path_for(chat)
+    Pidgin2Adium::ADIUM_LOG_DIRECTORY.join(
+      "#{chat.service}.#{chat.my_screen_name}",
+      chat.their_screen_name,
+      "#{chat.their_screen_name} (#{chat.start_time_xmlschema}).chatlog",
+      "#{chat.their_screen_name} (#{chat.start_time_xmlschema}).xml"
     )
-    allow(Pipio::Chat).to receive(:new).and_return(chat)
-
-    run_runner(path_to_directory)
-
-    path = "#{adium_log_directory}/aim.me/them/them (#{time}).chatlog/them (#{time}).xml"
-
-    expect(File.exist?(path)).to be true
   end
 end
